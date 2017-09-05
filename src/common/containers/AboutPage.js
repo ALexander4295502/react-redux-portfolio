25d7820412dcc6e16ab1e602290ebdd6e6b26c37
ALexander4295502 <yuanzhengstl@gmail.com>
2017-09-05 12:38:43 -0500

    add common actions and aboutpage container

---
+++ src/common/containers/AboutPage.js
@@ -0,0 +1,5 @@
+import {bindActionCreators} from 'redux';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import About from '../components/About';
+import * as AboutActions from '../actions/about';
