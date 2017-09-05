468056e53c77736c8e518e446868f7e41120719f
ALexander4295502 <yuanzhengstl@gmail.com>
2017-09-05 12:28:18 -0500

    setup page components

---
+++ src/common/components/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Banner from './layout/Banner';
+
+class Home extends Component {
+
+  constructor(props){
+    super(props);
+    this.eventToggleSidebar = this.eventToggleSidebar.bind(this)
+  }
+
+  eventToggleSidebar(e) {
+    e.preventDefault();
+    this.props.toggleSidebar(!this.props.layout.sidebarOpen)
+  }
+
+  render() {
+    return (
+      <Banner/>
+    );
+  }
+
+}
+
+export default Home;
---
+++ src/common/components/Services.js
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react';
+import Banner from './layout/Banner';
+
+
+class Services extends Component {
+
+  render () {
+    return (
+      <div>
+
+        <Banner />
+
+      </div>
+    );
+  }
+}
+
+export default Services;
