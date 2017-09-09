5094056c56e2c37562437f4812ecb5750a1281c1
ALexander4295502 <yuanzhengstl@gmail.com>
2017-09-09 11:13:32 -0500

    add tag for every Github repo item

--- src/common/components/about/Repos.js
+++ src/common/components/about/Repos.js
@@ -15,6 +15,16 @@ export default class Repos extends Component {
           <div className="repo-item" key={i}>
             <a href={repo.html_url} onClick={this.openNewPage}>{repo.name}</a><br/>
             <p>{repo.description}</p>
+            <ul className="tags">
+              <li>
+                <a href={`https://github.com/ALexander4295502/${repo.name}/search?l=${repo.language}`}
+                   className="tag"
+                   onClick={this.openNewPage}
+                >
+                  {repo.language === "HTML" ? "JavaScript" : repo.language}
+                </a>
+              </li>
+            </ul>
           </div>
         )}
       </div>
